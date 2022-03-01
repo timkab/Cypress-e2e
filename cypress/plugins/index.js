a2881626f7efacb74ff58e17d5cd3fe2e721d142
Timur <kabilov.t@gmail.com>
2022-02-28 17:55:17 -0800

    fix dotenv and export

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,14 +1,32 @@
-import dotenv from "dotenv";
+//require('@babel/register')({ presets: [['@babel/preset-env', { targets: { node: '8.17.0', }, },]], });
+//import dotenv from "dotenv";
+const dotenv = require('dotenv');
 
 const cucumber = require('cypress-cucumber-preprocessor').default;
+
+dotenv.config({ path: '.env.local' });
+dotenv.config();
+
+// Set Auth0 variables from .env
+
 module.exports = (on, config) => {
 
   on('file:preprocessor', cucumber())
+
+  config.env.auth0_username = process.env.AUTH0_USERNAME
+  config.env.auth0_password = process.env.AUTH0_PASSWORD
+  config.env.auth0_domain = process.env.AUTH0_DOMAIN
+  config.env.auth0_audience = process.env.AUTH0_AUDIENCE
+  config.env.auth0_scope = process.env.AUTH0_SCOPE
+  config.env.auth0_client_id = process.env.AUTH0_CLIENTID
+  config.env.auth0_client_secret = process.env.AUTH0_CLIENT_SECRETD
+
+  return config
+
 };
 
-// Setting Auth0 variables from .env
-dotenv.config({ path: ".env.local" });
-dotenv.config();
+
+/*
 
 export default (on, config) => {
 
@@ -22,3 +40,4 @@ export default (on, config) => {
 
   return config
 };
+*/
