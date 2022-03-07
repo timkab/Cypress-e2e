fe04eabf8049dce4e6db750dc1936d5ca2f92dd3
Timur <kabilov.t@gmail.com>
2022-03-07 09:33:11 -0800

    small fixes and updts

--- cypress/tests/IntroPage/Login/Login.js
+++ cypress/tests/IntroPage/Login/Login.js
@@ -5,7 +5,3 @@ import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 When('Passport Login', () => {
     cy.loginAuth0();
 })
-
-Then('shows onboarding', () => {
-    cy.contains('Your details').should('be.equal', 'Verified Income reports made easy - Steady Income Passport')
-})
