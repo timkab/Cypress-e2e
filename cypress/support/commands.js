c2f4a903699a2d55624697cb62d688e84c3827e5
Alex Kwiatkowski <akwiatkowski@steadyapp.com>
2022-06-02 16:37:19 -0600

    authenticate with auth0 and store result in local storage for income passport

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,67 +1,63 @@
 import * as jwt from "jsonwebtoken";
 
-Cypress.Commands.add(
-    'loginAuth0', () => {
-        const client_id = Cypress.env('auth0_client_id')
-        const client_secret = Cypress.env('auth0_client_secret')
-        const audience = Cypress.env('auth0_audience')
-        const scope = Cypress.env('auth0_scope')
-        const username = Cypress.env('auth0_username')
-        const password = Cypress.env('auth0_password')
-        const url = Cypress.env('auth0_domain')
-        //const base_url = Cypress.env('base_url')
+Cypress.Commands.add("loginAuth0", () => {
+  const client_id = Cypress.env("auth0_client_id");
+  const client_secret = Cypress.env("auth0_client_secret");
+  const audience = Cypress.env("auth0_audience");
+  const scope = Cypress.env("auth0_scope");
+  const username = Cypress.env("auth0_username");
+  const password = Cypress.env("auth0_password");
+  const url = Cypress.env("auth0_domain");
 
-        cy.log(`Logging in as ${username}`)
+  cy.log(`Logging in as ${username}`);
 
-        cy.request({
-            method: 'POST',
-            url,
-            body: {
-                grant_type: 'password',
-                username,
-                password,
-                audience,
-                scope,
-                client_id,
-                client_secret,
-            },
-        }).then(({ body }) => {
-            const claims = jwt.decode(body.id_token)
-            const {
-                nickname,
-                name,
-                picture,
-                updated_at,
-                email,
-                email_verified,
-                sub,
-                exp,
-            } = claims
+  cy.request({
+    method: "POST",
+    url,
+    body: {
+      grant_type: "password",
+      username,
+      password,
+      audience,
+      scope,
+      client_id,
+      client_secret,
+    },
+  }).then(({ body }) => {
+    const { payload: claims } = jwt.decode(body.id_token, { complete: true });
+    const {
+      nickname,
+      name,
+      picture,
+      updated_at,
+      email,
+      email_verified,
+      sub,
+      exp,
+    } = claims;
 
-            const item = {
-                body: {
-                    ...body,
-                    decodedToken: {
-                        claims,
-                        user: {
-                            nickname,
-                            name,
-                            picture,
-                            updated_at,
-                            email,
-                            email_verified,
-                            sub,
-                        },
-                        audience,
-                        client_id,
-                    },
-                },
-                expiresAt: exp,
-            }
+    const item = {
+      body: {
+        ...body,
+        decodedToken: {
+          claims,
+          user: {
+            nickname,
+            name,
+            picture,
+            updated_at,
+            email,
+            email_verified,
+            sub,
+          },
+          audience,
+          client_id,
+        },
+      },
+      expiresAt: exp,
+    };
 
-            window.localStorage.setItem('auth0Cypress', JSON.stringify(item))
-
-            cy.visit('/') //opens baseUrl specified in cypress.json
-        })
-    }
-)
+    window.localStorage.setItem("auth0Cypress", JSON.stringify(item));
+    cy.visit("/"); //opens baseUrl specified in cypress.json
+  });
+});
