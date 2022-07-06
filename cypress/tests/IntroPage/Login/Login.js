29b06032b5f65153f13758b6f9ac245a8c198985
Timur <kabilov.t@gmail.com>
2022-07-06 11:56:29 -0700

    some clean up

--- cypress/tests/IntroPage/Login/Login.js
+++ cypress/tests/IntroPage/Login/Login.js
@@ -1,7 +1,6 @@
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
-
 // calling custom command loginAuth0
-When('Passport Login', () => {
-    cy.loginAuth0();
-})
+When("Passport Login", () => {
+  cy.loginAuth0();
+});
